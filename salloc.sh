49d5568e93da7bfaa5de56c69e18f4905584347c
Andrew Chang <andrewch@login15.chn.perlmutter.nersc.gov>
2024-10-12 18:01:08 -0700

    salloc time to 10 mins default

--- salloc.sh
+++ salloc.sh
@@ -3,4 +3,4 @@ export OMP_NUM_THREADS=8
 export OMP_PROC_BIND=spread
 export OMP_PLACES=threads
 
-salloc -n 1 -N 1 --qos interactive -t 01:00:00 -C cpu -c 32 -J sph -A m4776
+salloc -n 1 -N 1 --qos interactive -t 00:10:00 -C cpu -c 32 -J sph -A m4776
